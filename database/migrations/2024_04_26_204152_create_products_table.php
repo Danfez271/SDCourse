8e77462692dbabadb8682e06d782ba25163676ea
Danfez <v-30616078@micorreo.uny.edu.ve>
2024-04-26 23:42:12 -0400

    Fixing error in db declaration

--- database/migrations/2024_04_26_204152_create_products_table.php
+++ database/migrations/2024_04_26_204152_create_products_table.php
@@ -20,10 +20,10 @@ return new class extends Migration
             $table->json('images')->nullable();
             $table->longText('description')->nullable();
             $table->decimal('price', 10, 2);
-            $table->boolean('is_active')->default('true');
-            $table->boolean('is_featured')->default('false');
-            $table->boolean('in_stock')->default('true');
-            $table->boolean('on_sale')->default('false');
+            $table->boolean('is_active')->default(true);
+            $table->boolean('is_featured')->default(false);
+            $table->boolean('in_stock')->default(true);
+            $table->boolean('on_sale')->default(false);
             $table->timestamps();
         });
     }
